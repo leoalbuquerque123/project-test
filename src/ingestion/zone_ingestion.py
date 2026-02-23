3e624eba86884956a9d045ed1387fbf30621ecf3
Leonardo Albuquerque <leoalbuquerque@icloud.com>
2026-02-22 21:18:12 -0300

    Refresh files

--- src/ingestion/zone_ingestion.py
+++ src/ingestion/zone_ingestion.py
@@ -12,16 +12,24 @@ GCS_PREFIX = "taxi_zone/"
 URL = "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
 output_path = "taxi_zone_lookup.csv"
 
+
 def download_file(URL, output_path):
+    """
+    Download CSV file.
+    """
     response = requests.get(URL)
     response.raise_for_status()
 
     with open(output_path, "wb") as f:
         f.write(response.content)
 
-    print("Download Taxi Zone Lookup complete!")
+    print("Download Taxi Zone Lookup completed!")
+
 
 def get_gsc_client(CREDENTIALS_PATH):
+    """
+    Creates and returns a GCS client authenticated with the service account.
+    """
     credentials = service_account.Credentials.from_service_account_file(
         CREDENTIALS_PATH,
         scopes=['https://www.googleapis.com/auth/cloud-platform']
@@ -29,13 +37,22 @@ def get_gsc_client(CREDENTIALS_PATH):
     print("Conection OK!")
     return storage.Client(credentials=credentials, project=credentials.project_id)
 
+
 def upload_file_to_gcs(client, GCS_BUCKET_NAME, GCS_PREFIX, output_path):
+    """
+    Uploads a file to Google Cloud Storage.
+    """
     bucket = client.bucket(GCS_BUCKET_NAME)
     blob = bucket.blob(GCS_PREFIX + output_path)
     blob.upload_from_filename(output_path)
     print(f"File uploaded to gs://{GCS_BUCKET_NAME}/{GCS_PREFIX}{output_path}")
 
 
-download_file(URL, output_path)
-client = get_gsc_client(CREDENTIALS_PATH)
-upload_file_to_gcs(client, GCS_BUCKET_NAME, GCS_PREFIX, output_path)
+def main():
+    download_file(URL, output_path)
+    client = get_gsc_client(CREDENTIALS_PATH)
+    upload_file_to_gcs(client, GCS_BUCKET_NAME, GCS_PREFIX, output_path)
+
+
+if __name__ == "__main__":
+    main()
